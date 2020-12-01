0c22c8fe437b0b9ad905977d00b1b38b347b93b9
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-01 09:51:29 +0100

    part two done

--- dayOne.js
+++ dayOne.js
@@ -1,18 +1,40 @@
-import { readIntFile } from './util.js';
+import { readIntFile, print } from './util.js';
 
 const puzzleInput = readIntFile('./input/dayOne');
 
-let firstEntry = 0;
-let secondEntry = 0;
+function partOne() {
+  let firstEntry, secondEntry = 0;
+  
+  puzzleInput.some(num => {
+    firstEntry = num;
+    let second = puzzleInput.filter(elem => (firstEntry + elem) === 2020);
+    if (second.length > 0) {
+      secondEntry = second[0];
+      return true;
+    }
+  });
+  
+  console.log({ firstEntry, secondEntry });
+  console.log(firstEntry * secondEntry);
+}
 
-puzzleInput.some(num => {
-  firstEntry = num;
-  let second = puzzleInput.filter(elem => (firstEntry + elem) === 2020);
-  if (second.length > 0) {
-    secondEntry = second[0];
-    return true;
-  }
-});
+function partTwo() {
+  let firstEntry, secondEntry, thirdEntry = 0;
 
-console.log({ firstEntry, secondEntry });
-console.log(firstEntry * secondEntry);
+  puzzleInput.some(first => {
+    puzzleInput.forEach(second => {
+      let third = puzzleInput.filter(elem => (first + second + elem) === 2020);
+      if (third.length > 0) {
+        firstEntry = first;
+        secondEntry = second;
+        thirdEntry = third[0];
+      }
+      return !firstEntry;
+    })
+  })
+
+  console.log({ firstEntry, secondEntry, thirdEntry });
+  console.log(firstEntry * secondEntry * thirdEntry);
+}
+
+partTwo();
