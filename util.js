2bca40202cd248964a834a53fd7c47774c444b35
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-01 09:38:37 +0100

    Read lines from puzzle input

---
+++ util.js
@@ -0,0 +1,10 @@
+import { readFileSync } from 'fs';
+
+export function readLines(path) {
+  const file = readFileSync(path, 'utf-8');
+  return file.split('\r\n');
+}
+
+export function readIntFile(path) {
+  return readLines(path).map(elem => Number.parseInt(elem));
+}
