1cfb8c10a2cb89a8aa3eb8070a11dc18e2b7e1d9
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-07 09:30:21 +0100

    template

--- days/template.js
+++ days/template.js
@@ -3,7 +3,9 @@ import {} from '../util';
 const puzzleInput = []
 
 const partOne = () => {
+  let t1 = new Date().getTime();
 
+  let t2 = new Date().getTime();
   console.log('\n');
   console.log('   Part One:');
   console.log(` Done in ${t2-t1} ms`);
@@ -12,7 +14,9 @@ const partOne = () => {
 }
 
 const partTwo = () => {
+  let t1 = new Date().getTime();
 
+  let t2 = new Date().getTime();
   console.log('\n');
   console.log('   Part Two:');
   console.log(` Done in ${t2-t1} ms`);
