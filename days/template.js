b1c8ecc1814eecf1c632eb71f34e20df275117d5
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-07 09:14:22 +0100

    template

---
+++ days/template.js
@@ -0,0 +1,24 @@
+import {} from '../util';
+
+const puzzleInput = []
+
+const partOne = () => {
+
+  console.log('\n');
+  console.log('   Part One:');
+  console.log(` Done in ${t2-t1} ms`);
+  console.log('ans');  
+  console.log('\n');
+}
+
+const partTwo = () => {
+
+  console.log('\n');
+  console.log('   Part Two:');
+  console.log(` Done in ${t2-t1} ms`);
+  console.log('ans');  
+  console.log('\n');
+}
+
+partOne();
+partTwo();
