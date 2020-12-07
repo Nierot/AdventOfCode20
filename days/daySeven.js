41b9f44e17ee3e9b1fb0cff281f0ace7d99a2234
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-07 09:30:33 +0100

    daySeven parsing

---
+++ days/daySeven.js
@@ -0,0 +1,36 @@
+import { readLines } from '../util';
+
+const puzzleInput = readLines('./input/daySeven');
+
+const splitBags = () => {
+  let oof = puzzleInput[1]
+    .split(/contain|\.|,/)
+    .filter(word => word !== '')
+    .map(word => word[0] === ' ' ? word.slice(1) : word)
+    .map(word => word[word.length - 1] === ' ' ? word.slice(0, word.length - 1) : word)
+    // .map(word => word[word.length - 1] === ' ' ? word.split(' ').filter(word => word !== '') : word)
+    // .map(word => word[0] === ' ' ? word.split(' ').slice(1) : word)
+  console.log(oof)
+}
+
+const partOne = () => {
+
+  console.log('\n');
+  console.log('   Part One:');
+  console.log(` Done in ${t2-t1} ms`);
+  console.log('ans');  
+  console.log('\n');
+}
+
+const partTwo = () => {
+
+  console.log('\n');
+  console.log('   Part Two:');
+  console.log(` Done in ${t2-t1} ms`);
+  console.log('ans');  
+  console.log('\n');
+}
+
+splitBags();
+// partOne();
+// partTwo();
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require = require('esm')(module);
 
 // Load this days module
-module.exports = require('./days/daySix.js');
+module.exports = require('./days/daySeven.js');
