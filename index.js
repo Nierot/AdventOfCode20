d6997915e7d3ff993eea06997816d48b0bfa148d
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-08 11:52:36 +0100

    time everything

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 require = require('esm')(module);
 
 // Load this days module
-module.exports = require('./days/day8.js');
+// Comment this out if you run everything
+// module.exports = require('./days/day8.js');
 
 const runEverything = () => {
   const fs = require('fs');
+  let t1 = new Date().getTime();
   fs.readdirSync('./days')
     .filter(file => file.includes('.js'))
     .filter(file => file !== 'template.js')
+    .sort((a, b) => a.split('day')[1] - b.split('day')[1])
     .forEach(file => {
       console.log(file);
       require(`./days/${file}`)
     })
+  let t2 = new Date().getTime();
+  console.log('\n', `Ran everything in ${t2-t1} ms`);
 }
 
 runEverything();
