563c2cf813bff4ca25cd0063502408ebd1512c40
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-10 17:38:18 +0100

    Part one done

--- days/day10.js
+++ days/day10.js
@@ -1,22 +1,44 @@
 import { readIntFile } from '../util';
 
-const puzzleInput = readIntFile('./input/testInput')
+const puzzleInput = readIntFile('./input/dayTen')
   .sort((a,b) => a - b);
 const highestJolts = Math.max(...puzzleInput);
 const deviceJolts = highestJolts + 3;
 
-const checkAdapters = (input) => {
-  
+let previous = 0;
+
+const checkAdapters = () => {
+  let ones = 0;
+  let threes = 1;
+  puzzleInput.filter(jolt => {
+    console.log(jolt);
+    if (jolt === (previous + 1)) {
+      previous = jolt;
+      ones++;
+      return true;
+    } else if (jolt === (previous + 2)) {
+      previous = jolt;
+      return true;
+    } else if (jolt === (previous + 3)) {
+      previous = jolt;
+      threes++;
+      return true;
+    } else {
+      return false;
+    }
+  })
+  return { ones, threes }
 }
 
 const partOne = () => {
   let t1 = new Date().getTime();
-  checkAdapters();
+  let { ones, threes } = checkAdapters();
+  let ans = ones * threes;
   let t2 = new Date().getTime();
   console.log('\n');
   console.log('   Part One:');
   console.log(` Done in ${t2-t1} ms`);
-  console.log('ans');  
+  console.log({ ans });  
   console.log('\n');
 }
 
